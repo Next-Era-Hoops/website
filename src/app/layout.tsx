6e974bfd01aab19189971f5b9b18b8d0ba064cd4
Ryan Malani <ryanmalani24@gmail.com>
2025-05-12 11:16:46 -0400

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "leaflet/dist/leaflet.css"
 import type React from "react"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -14,6 +15,7 @@ export default function RootLayout({
       <body className="font-eurostile bg-black">
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
